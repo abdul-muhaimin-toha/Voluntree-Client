d61a4964dff07fa49920cae44f8e73041655b622
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-05-11 18:55:50 +0600

    delete post doned

--- src/components/MyPostTableRow.jsx
+++ src/components/MyPostTableRow.jsx
@@ -1,11 +1,54 @@
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
 import { FaPenToSquare, FaTrash } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const MyPostTableRow = ({ post, index }) => {
   const { _id, title, category, deadline, location, volunteers_needed } =
     post || {};
 
-  console.log(deadline);
+  const handleDeletePost = (e) => {
+    e.preventDefault();
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#4262FF",
+      cancelButtonColor: "#000",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(`http://localhost:3000/volunteers/${_id}`)
+          .then((response) => {
+            console.log(response.data);
+            if (response.data.deletedCount > 0) {
+              toast("Post deleted successfully", {
+                icon: "👏",
+                style: {
+                  borderRadius: "10px",
+                  background: "#4262FF",
+                  color: "#fff",
+                },
+              });
+            }
+          })
+          .catch(function (error) {
+            console.error(error.message);
+            toast("Delete failed, try again!", {
+              icon: "❌",
+              style: {
+                borderRadius: "10px",
+                background: "#4262FF",
+                color: "#fff",
+              },
+            });
+          });
+      }
+    });
+  };
 
   return (
     <tr className="dark:text-white ">
@@ -18,12 +61,14 @@ const MyPostTableRow = ({ post, index }) => {
       <td className="text-center text-base text-primary">
         <button>
           <Link to={`/update-my-post/${_id}`}>
-            <FaPenToSquare />
+            <FaPenToSquare className="m-2" />
           </Link>
         </button>
       </td>
       <td className="text-center text-base text-primary">
-        <FaTrash />
+        <button onClick={handleDeletePost}>
+          <FaTrash className="m-2" />
+        </button>
       </td>
     </tr>
   );
